da6609bfddbeaf1842e0fae438ef70fa96e9b66e
casey.Q.Wood <caseywood252@gmail.com>
2021-07-06 13:46:10 -0700

    interviewerlistitem and initial interviewerlist

---
+++ src/components/InterviewListItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import 'components/InterviewListItem.scss'
+import classnames from 'classnames'
+
+export default function interviewListItem(props) {
+
+  const className = classnames(
+    "interviewers__item",
+    {'interviewers__item--selected': props.selected
+  })
+
+  return (
+  <li className={className} onClick={() => {props.setInterviewer(props.name) }}>
+    <img
+      key={props.id}
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+    />
+    {props.selected && props.name}
+  </li>
+  )
+}
---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "components/InterviewerList"
+
+export default function interviewerList(props) {
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  )
+}
